12dd3f9a0bdac859d7f6e55e30185a82b17206bd
BRIAN MURITHI <97604113+brayohmurithidev@users.noreply.github.com>
2023-01-24 01:03:52 +0300

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""index.py to connect to API"""
+"""index.py to connect to Application Programming Interface"""
 from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify
 from models import storage
